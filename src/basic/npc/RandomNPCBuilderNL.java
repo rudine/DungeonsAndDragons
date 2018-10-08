1006a6ceda17063604883086a6acf1d055075ccd
Rudine Bijlsma <rudine.bijlsma@topicus.nl>
2018-10-08 17:42:56 +0200

    NPC-Builder completely translated into Dutch

--- src/basic/npc/RandomNPCBuilderNL.java
+++ src/basic/npc/RandomNPCBuilderNL.java
@@ -8,14 +8,14 @@ import basic.services.DiceService;
 public abstract class RandomNPCBuilderNL {
 	
 	private static Map<Integer, String> uiterlijk= new HashMap<>();
-	private static Map<Integer, String> highAbilities= new HashMap<>();
-	private static Map<Integer, String> lowAbilities= new HashMap<>();
-	private static Map<Integer, String> talents= new HashMap<>();
-	private static Map<Integer, String> mannerisms= new HashMap<>();
-	private static Map<Integer, String> interactionTraits= new HashMap<>();
-	private static Map<Integer, String> ideals= new HashMap<>();
-	private static Map<Integer, String> bonds= new HashMap<>();
-	private static Map<Integer, String> flaws= new HashMap<>();
+	private static Map<Integer, String> hogeAbilities= new HashMap<>();
+	private static Map<Integer, String> lageAbilities= new HashMap<>();
+	private static Map<Integer, String> talenten= new HashMap<>();
+	private static Map<Integer, String> maniertjes= new HashMap<>();
+	private static Map<Integer, String> omgangsVormen = new HashMap<>();
+	private static Map<Integer, String> idealen= new HashMap<>();
+	private static Map<Integer, String> obligaties= new HashMap<>();
+	private static Map<Integer, String> gebreken= new HashMap<>();
 	private static Map<Integer, String> alignments= new HashMap<>();
 	
 	public static void main(String[] args) {
@@ -24,14 +24,14 @@ public abstract class RandomNPCBuilderNL {
 	
 	private static void initTables() {
 		initUiterlijk();
-		initHighAbility();
-		initLowAbility();
-		initTalents();
-		initMannerisms();
-		initInteractionTraits();
-		initIdeals();
-		initBonds();
-		initFlaws();
+		initHoogsteAbility();
+		initLaagsteAbility();
+		initTalenten();
+		initManiertjes();
+		initOmgangsvormen();
+		initIdealen();
+		initObligaties();
+		initGebreken();
 		initAlignment();
 	}
 	
@@ -51,127 +51,127 @@ public abstract class RandomNPCBuilderNL {
 		uiterlijk.put(13, "Kaal");
 		uiterlijk.put(14, "Gevlochten baard of haar");
 		uiterlijk.put(15, "Ongebruikelijke haarkleur");
-		uiterlijk.put(16, "Trekkend oog");
+		uiterlijk.put(16, "Trekkend oog / mondhoek / hoofd");
 		uiterlijk.put(17, "Opvallende neus");
-		uiterlijk.put(18, "Opvallende houding (stijf of gebogen bijv.)");
+		uiterlijk.put(18, "Opvallende houding (stijf of krom bijv.)");
 		uiterlijk.put(19, "Een schoonheid / adonis");
 		uiterlijk.put(20, "Erg lelijk");
 	}
 	
-	private static void initHighAbility() {
-		highAbilities.put(1, "Strength - machtig, vol bravoure, beresterk");
-		highAbilities.put(2, "Dexterity - lichtvoetig, lenig, gracieus");
-		highAbilities.put(3, "Constitution - hardy, hale, gezond"); //Hier was ik gebleven
-		highAbilities.put(4, "Intelligence - studious, learned, inquisitive");
-		highAbilities.put(5, "Wisdom - perceptive, spiritual, insightful");
-		highAbilities.put(6, "Charisma - persuasive, forceful, born leader");
+	private static void initHoogsteAbility() {
+		hogeAbilities.put(1, "Strength - machtig, vol bravoure, beresterk");
+		hogeAbilities.put(2, "Dexterity - lichtvoetig, lenig, gracieus");
+		hogeAbilities.put(3, "Constitution - taai, energiek, gezond"); 
+		hogeAbilities.put(4, "Intelligence - leergierig, geleerd, onderzoekend");
+		hogeAbilities.put(5, "Wisdom - opmerkzaam, spiritueel, wijs");
+		hogeAbilities.put(6, "Charisma - overtuigend, assertief, geboren leider");
 	}
 	
-	private static void initLowAbility() {
-		lowAbilities.put(1, "Strength - feeble, scrawny");
-		lowAbilities.put(2, "Dexterity - clumsy, fumbling");
-		lowAbilities.put(3, "Constitution - sickly, pale");
-		lowAbilities.put(4, "Intelligence - dim-witted, slow");
-		lowAbilities.put(5, "Wisdom - oblivious, absentminded");
-		lowAbilities.put(6, "Charisma - dull, boring");	
+	private static void initLaagsteAbility() { 
+		lageAbilities.put(1, "Strength - zwak, mager");
+		lageAbilities.put(2, "Dexterity - onhandig, klungelig");
+		lageAbilities.put(3, "Constitution - ziekelijk, bleek");
+		lageAbilities.put(4, "Intelligence - dom, langzaam");
+		lageAbilities.put(5, "Wisdom - vergeetachtig, verstrooid");
+		lageAbilities.put(6, "Charisma - simpel, saai");	
 	}
 	
-	private static void initTalents() {
-		talents.put(1, "Plays a musical instrument");
-		talents.put(2, "Speaks several languages fluently");
-		talents.put(3, "Unbelievably lucky");
-		talents.put(4, "Perfect memory");
-		talents.put(5, "Great with animals");
-		talents.put(6, "Great with children");
-		talents.put(7, "Great at one game");
-		talents.put(8, "Great at impersonations");
-		talents.put(9, "Draws beautifully");
-		talents.put(10, "Paints beautifully");
-		talents.put(11, "Sings beautifully");
-		talents.put(12, "Drinks everyone under the table");
-		talents.put(13, "Expert carpenter");
-		talents.put(14, "Expert cook");
-		talents.put(15, "Expert dart thrower / stone skipper");
-		talents.put(16, "Great at solving puzzles");
-		talents.put(17, "Expert juggler");
-		talents.put(18, "Skilled actor and master of disguise");
-		talents.put(19, "Skilled dancer");
-		talents.put(20, "Knows thieves' cant");
+	private static void initTalenten() {
+		talenten.put(1, "Speelt een muziekinstrument");
+		talenten.put(2, "Kan verschillende talen vloeiend spreken");
+		talenten.put(3, "Guus geluk");
+		talenten.put(4, "Perfect geheugen");
+		talenten.put(5, "Goed met dieren");
+		talenten.put(6, "Goed met kinderen");
+		talenten.put(7, "Goed in één spel");
+		talenten.put(8, "Goed in mensen nadoen");
+		talenten.put(9, "Tekent mooi");
+		talenten.put(10, "Schildert mooi");
+		talenten.put(11, "Zingt mooi");
+		talenten.put(12, "Drinkt iedereen onder de tafel");
+		talenten.put(13, "Timmerexpert");
+		talenten.put(14, "Kan goed koken");
+		talenten.put(15, "Goed in het gooien van darts / gooien van steentjes op water");
+		talenten.put(16, "Kan goed puzzels oplossen");
+		talenten.put(17, "Jongleur");
+		talenten.put(18, "Goede acteur en goed in vermommingen");
+		talenten.put(19, "Danst goed");
+		talenten.put(20, "Kent thieves' cant");
 	}
 	
-	private static void initMannerisms() {
-		mannerisms.put(1, "Prone to singing, whistling or humming quietly");
-		mannerisms.put(2, "Speaks in rhyme or some other peculiar way");
-		mannerisms.put(3, "Particulary low or high voice");
-		mannerisms.put(4, "Slurs words, lisps or stutters");
-		mannerisms.put(5, "Enunciates overly clearly");
-		mannerisms.put(6, "Speaks loudly");
-		mannerisms.put(7, "Whispers");
-		mannerisms.put(8, "Uses flowery speach or long words");
-		mannerisms.put(9, "Frequently uses the wrong words");
-		mannerisms.put(10, "Uses colorful oaths and exclamations");
-		mannerisms.put(11, "Makes constant jokes or puns");
-		mannerisms.put(12, "Prone to predications of doom");
-		mannerisms.put(13, "Fidgets");
-		mannerisms.put(14, "Squints");
-		mannerisms.put(15, "Stares into the distance");
-		mannerisms.put(16, "Chews something");
-		mannerisms.put(17, "Paces");
-		mannerisms.put(18, "Taps fingers");
-		mannerisms.put(19, "Bites fingernails");
-		mannerisms.put(20, "Twirls hair or tugs beard");
+	private static void initManiertjes() {
+		maniertjes.put(1, "Neuriet, fluit of zingt vaak zomaar");
+		maniertjes.put(2, "Heeft een aparte manier van spreken, (rijmen bijv.)");
+		maniertjes.put(3, "Bijzonder hoge of lage stem");
+		maniertjes.put(4, "Verbasterd woorden, slist of stottert");
+		maniertjes.put(5, "Articuleert uitzonderlijk duidelijk");
+		maniertjes.put(6, "Spreekt erg luid");
+		maniertjes.put(7, "Fluistert");
+		maniertjes.put(8, "Gebruikt archaïsche taal en moeilijke woorden");
+		maniertjes.put(9, "Gebruikt vaak de verkeerde woorden");
+		maniertjes.put(10, "Heeft bijzondere uitroepen en vloekt kleurrijk");
+		maniertjes.put(11, "Grappenmaker");
+		maniertjes.put(12, "Doemdenker");
+		maniertjes.put(13, "Frunnikt");
+		maniertjes.put(14, "Knijpt ogen samen");
+		maniertjes.put(15, "Staart in de verte");
+		maniertjes.put(16, "Kauwt op iets");
+		maniertjes.put(17, "Ijsbeert");
+		maniertjes.put(18, "Trommelt met de vingers");
+		maniertjes.put(19, "Nagelbijter");
+		maniertjes.put(20, "Draait aan haar of plukt in baard");
 	}
 	
-	private static void initInteractionTraits() {
-		interactionTraits.put(1, "Argumentative");
-		interactionTraits.put(2, "Arrogant");
-		interactionTraits.put(3, "Blustering");
-		interactionTraits.put(4, "Rude");
-		interactionTraits.put(5, "Curious");
-		interactionTraits.put(6, "Friendly");
-		interactionTraits.put(7, "Honest");
-		interactionTraits.put(8, "Hot tempered");
-		interactionTraits.put(9, "Irritable");
-		interactionTraits.put(10, "Ponderous");
-		interactionTraits.put(11, "Quiet");
-		interactionTraits.put(12, "Suspicious");
+	private static void initOmgangsvormen() {
+		omgangsVormen .put(1, "Betogend");
+		omgangsVormen .put(2, "Arrogant");
+		omgangsVormen .put(3, "Opschepper");
+		omgangsVormen .put(4, "Onbeleefd");
+		omgangsVormen .put(5, "Nieuwsgierig");
+		omgangsVormen .put(6, "Vriendelijk");
+		omgangsVormen .put(7, "Eerlijk");
+		omgangsVormen .put(8, "Temperamentvol");
+		omgangsVormen .put(9, "Prikkelbaar");
+		omgangsVormen .put(10, "Zwaarwichtig");
+		omgangsVormen .put(11, "Stil");
+		omgangsVormen .put(12, "Wantrouwig");
 	}
 	
-	private static void initIdeals() {
-		ideals.put(1, "G: Beauty, E: Domination, L: Community, C: Change, N: Balance, O: Aspiration");
-		ideals.put(2, "G: Charity, E: Greed, L: Fairness, C: Creativity, N: Knowledge, O: Discovery");
-		ideals.put(3, "G: Greater good, E: Might, L: Honor, C: Freedom, N: Live and let live, O: Glory");
-		ideals.put(4, "G: Life, E: Pain, L: Logic, C: Independence, N: Moderation, O: Nation");
-		ideals.put(5, "G: Respect, E: Retribution, L: Responsibility, C: No limits, N: Neutrality, O: Redemption");
-		ideals.put(6, "G: Self-sacrifice, E: Slaughter, L: Tradition, C: Whimsy, N: People, O: Self-knowledge");
+	private static void initIdealen() {
+		idealen.put(1, "G: Schoonheid, E: Overheersing, L: Harmonie, C: Verandering, N: Balans, O: Ambitie");
+		idealen.put(2, "G: Vrijgevigheid, E: Hebzucht, L: Eerlijkheid, C: Creativiteit, N: Kennis, O: Vindingrijkheid");
+		idealen.put(3, "G: Grotere goed, E: Macht, L: Eer, C: Vrijheid, N: Leef en laat leven, O: Glorie");
+		idealen.put(4, "G: Leven, E: Pijn, L: Logica, C: Onafhankelijkheid, N: Matigheid, O: Voor het volk");
+		idealen.put(5, "G: Respect, E: Vergelding, L: Verantwoordelijkheid, C: Geen grenzen, N: Onpartijdigheid, O: Verlossing");
+		idealen.put(6, "G: Zelfopoffering, E: Afslachting, L: Traditie, C: Originaliteit, N: Mensen, O: Zelf-kennis");
 	}
 	
-	private static void initBonds() {
-		bonds.put(1, "Dedicated to fulfilling a personal life goal");
-		bonds.put(2, "Protective of close family members");
-		bonds.put(3, "Protective of collegues or compatriots");
-		bonds.put(4, "Loyal to a benefactor, patron or employer");
-		bonds.put(5, "Captivated by a romantic interest");
-		bonds.put(6, "Drawn to a special place");
-		bonds.put(7, "Protective of a sentimental keepsake");
-		bonds.put(8, "Protective of a valuable possession");
-		bonds.put(9, "Out for revenge");
-		bonds.put(10, "Roll twice ignoring results of 10");
+	private static void initObligaties() {
+		obligaties.put(1, "Toegewijd aan het vervullen van een levensdoel");
+		obligaties.put(2, "Beschermt haar / zijn gezin");
+		obligaties.put(3, "Beschermt haar / zijn collega's of compagnons");
+		obligaties.put(4, "Loyaal aan een beschermheer/dame, weldoener of werkgever");
+		obligaties.put(5, "Verliefd");
+		obligaties.put(6, "Hang naar een bepaalde speciale plek");
+		obligaties.put(7, "Beschermt een sentimenteel aandenken");
+		obligaties.put(8, "Beschermt een kostbaar bezit");
+		obligaties.put(9, "Uit op wraak");
+		obligaties.put(10, "Rol twee keer, negeer 10 daarbij");
 	}
 	
-	private static void initFlaws() {
-		flaws.put(1, "Forbidden love or susceptibility to romance");
-		flaws.put(2, "Enjoys decadent pleasures");
-		flaws.put(3, "Arrogance");
-		flaws.put(4, "Envies another creature's possesions or station");
-		flaws.put(5, "Overpowering greed");
-		flaws.put(6, "Prone to rage");
-		flaws.put(7, "Has a powerful enemy");
-		flaws.put(8, "Specific phobia");
-		flaws.put(9, "Shameful or scandalous history");
-		flaws.put(10, "Secret crime or misdeed");
-		flaws.put(11, "Possesion of forbidden lore");
-		flaws.put(12, "Foolhardy bravery");
+	private static void initGebreken() {
+		gebreken.put(1, "Minnaar en / of snel verliefd");
+		gebreken.put(2, "Heeft dure hobbies");
+		gebreken.put(3, "Arrogantie");
+		gebreken.put(4, "Jaloers op iemands bezit of status");
+		gebreken.put(5, "Ontzettend hebzuchtig");
+		gebreken.put(6, "Wordt snel kwaad");
+		gebreken.put(7, "Heeft een machtige vijand");
+		gebreken.put(8, "Heeft last van een bepaalde angst");
+		gebreken.put(9, "Schandelijk verleden");
+		gebreken.put(10, "Verbergt een misdaad");
+		gebreken.put(11, "Bezit verloren kennis");
+		gebreken.put(12, "Nietsonziende heldhaftigheid");
 	}
 	
 	private static void initAlignment() {
@@ -191,23 +191,23 @@ public abstract class RandomNPCBuilderNL {
 		initTables();
 		StringBuilder npcBuilder = new StringBuilder();
 		npcBuilder.append("Alignment: \t\t" + alignments.get(DiceService.throwD6(1)) + "\n");
-		npcBuilder.append("Appearance: \t\t" + uiterlijk.get(DiceService.throwD20(1)) + "\n");
+		npcBuilder.append("Uiterlijk: \t\t" + uiterlijk.get(DiceService.throwD20(1)) + "\n");
 		
 		int rollForHighAbility = DiceService.throwD6(1);
-		npcBuilder.append("High ability: \t\t" + highAbilities.get(rollForHighAbility) + "\n");
+		npcBuilder.append("Hoge ability: \t\t" + hogeAbilities.get(rollForHighAbility) + "\n");
 		
 		int rollForLowAbility = DiceService.throwD6(1);
 		while (rollForLowAbility == rollForHighAbility) {
 			rollForLowAbility = DiceService.throwD6(1);
 		}
-		npcBuilder.append("Low ability: \t\t" + lowAbilities.get(rollForLowAbility) + "\n");
+		npcBuilder.append("Lage ability: \t\t" + lageAbilities.get(rollForLowAbility) + "\n");
 		
-		npcBuilder.append("Talent: \t\t" + talents.get(DiceService.throwD20(1)) + "\n");
-		npcBuilder.append("Mannerism: \t\t" + mannerisms.get(DiceService.throwD20(1)) + "\n");
-		npcBuilder.append("Iteraction w. others: \t" + interactionTraits.get(DiceService.throwD12(1)) + "\n");
-		npcBuilder.append("Ideal: \t\t\t" + ideals.get(DiceService.throwD6(1)) + "\n");
+		npcBuilder.append("Talent: \t\t" + talenten.get(DiceService.throwD20(1)) + "\n");
+		npcBuilder.append("Maniertje(s): \t\t" + maniertjes.get(DiceService.throwD20(1)) + "\n");
+		npcBuilder.append("Omgang met anderen: \t" + omgangsVormen .get(DiceService.throwD12(1)) + "\n");
+		npcBuilder.append("Ideaal: \t\t" + idealen.get(DiceService.throwD6(1)) + "\n");
 		getBondsText(npcBuilder);
-		npcBuilder.append("Flaw or secret: \t" + flaws.get(DiceService.throwD12(1)) + "\n");
+		npcBuilder.append("Geheim of gebrek: \t" + gebreken.get(DiceService.throwD12(1)) + "\n");
 		
 		return npcBuilder.toString();
 	}
@@ -218,7 +218,7 @@ public abstract class RandomNPCBuilderNL {
 			for(int i = 0 ; i<2 ; i++) {
 				rollForBonds = DiceService.throwD10(1);
 				if(rollForBonds < 10) {
-					npcBuilder.append("Bond: \t\t\t" + bonds.get(rollForBonds) + "\n");
+					npcBuilder.append("Obligatie: \t\t" + obligaties.get(rollForBonds) + "\n");
 				}
 				else {
 					i--;
@@ -227,7 +227,7 @@ public abstract class RandomNPCBuilderNL {
 			}
 		}
 		else {
-			npcBuilder.append("Bond: \t\t\t" + bonds.get(rollForBonds) + "\n");
+			npcBuilder.append("Obligatie: \t\t" + obligaties.get(rollForBonds) + "\n");
 		}
 	}
 }
