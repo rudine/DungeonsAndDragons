8fae023ff4ef3b68591691022829dc74b5b7bfe6
Rudine Bijlsma <rudine.bijlsma@topicus.nl>
2018-10-17 21:44:44 +0200

    NightMare enemy WIP

---
+++ src/basic/monsters/NightMare.java
@@ -0,0 +1,42 @@
+package basic.monsters;
+
+import basic.attacktypes.MeleeAttack;
+import basic.attacktypes.MeleeAttackBuilder;
+import basic.monsters.specialabilities.SpecialAbility;
+import basic.ruleobjects.AbilityScores;
+import basic.ruleobjects.DamageType;
+
+public class NightMare extends AbstractEnemy {
+	
+	public NightMare() {
+		setAC(13);
+		setHitpoints(68);
+		setSpeed("60 ft. 12v, fly 90ft. 18v");
+		setAbilityScores(new AbilityScores(18, 15, 16, 10, 13, 15));
+		setAttacksOnAttackAction(1);
+		setAttacks();
+		// special abiltiy confer fire resistance
+		// special ability illumination
+		// special action (attack) Etherial Stride
+	}
+
+	private void setAttacks() {
+		MeleeAttackBuilder builder = new MeleeAttackBuilder();
+		MeleeAttack hooves = builder.setWeaponName("Hooves")//
+				.setToHit(6)//
+				.setBaseDamage(4 + 7)// FIXME plus 7 fire damage
+				.setDamageDie(8)//
+				.setTimesToThrowDamageDie(2)//
+				.setDamageType(DamageType.BLUDGEONING)//
+				.build();
+		addToAvailableAttacks(hooves);
+	}
+	
+	@Override
+	public void doDamage(int damage, DamageType type) {
+		if(type == DamageType.FIRE)
+			damage = 0;
+		super.doDamage(damage, type);
+	}
+	
+}
