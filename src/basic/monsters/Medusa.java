6ed8dcbe1c12d8d0b0a21d8e8e99dcf5d0a1fb31
Rudine Bijlsma <rudine@gmail.com>
2019-02-26 16:45:04 +0100

    Medusa has actions instead of a special attack and set multiattacks so that they are displayed right

--- src/basic/monsters/Medusa.java
+++ src/basic/monsters/Medusa.java
@@ -1,5 +1,7 @@
 package basic.monsters;
 
+import basic.action.Action;
+import basic.action.ActionBuilder;
 import basic.attack.DamageComponent;
 import basic.attack.types.MeleeAttack;
 import basic.attack.types.RangedAttack;
@@ -10,6 +12,7 @@ import basic.ruleobjects.AbilityTypes;
 import basic.ruleobjects.Alignment;
 import basic.ruleobjects.DamageType;
 import basic.ruleobjects.MonsterType;
+import basic.ruleobjects.SavingThrow;
 import basic.ruleobjects.Size;
 import basic.ruleobjects.Skill;
 import basic.ruleobjects.modifiers.SkillModifier;
@@ -26,9 +29,8 @@ public class Medusa extends AbstractEnemy{
 		addToSkills(new SkillModifier(4, Skill.Perception, AbilityTypes.WIS));
 		addToSkills(new SkillModifier(5, Skill.Stealth, AbilityTypes.DEX));
 		setAttacks();
-		setAttacksOnAttackAction(3);
-		setSpecialAttacks();
-		setSpecialAttacksOnAction(1);
+		setAttacksOnAttackAction(5); // TODO 2 ranged or 3 melee, hack to make it work
+		setActions();
 		setMonsterType(MonsterType.monstrosity);
 		setSize(Size.Medium);
 		setAlignment(Alignment.LawfulEvil);
@@ -38,6 +40,7 @@ public class Medusa extends AbstractEnemy{
 		MeleeAttack shortsword = new MeleeAttackBuilder()//
 				.setWeaponName("shortsword")//
 				.setToHit(5)
+				.setUsesOnMultiAttack(2)
 				.addDamageComponent(new DamageComponent(1, 6, 2, DamageType.PIERCING))//
 				.build();
 		addToAvailableAttacks(shortsword);
@@ -45,6 +48,7 @@ public class Medusa extends AbstractEnemy{
 		MeleeAttack snakeHair = new MeleeAttackBuilder()//
 				.setWeaponName("snake hair")//
 				.setToHit(5)
+				.setUsesOnMultiAttack(1)
 				.addDamageComponent(new DamageComponent(1, 4, 2, DamageType.PIERCING))//
 				.addDamageComponent(new DamageComponent(4, 6, 0, DamageType.POISON))//
 				.build();
@@ -55,11 +59,36 @@ public class Medusa extends AbstractEnemy{
 				.setWeaponName("longbow")//
 				.addDamageComponent(new DamageComponent(1, 8, 2, DamageType.PIERCING))//
 				.addDamageComponent(new DamageComponent(2, 6, 0, DamageType.POISON))//
+				.setRange("150/600ft.")//
 				.build();
+		longbow.setNumberOfUsesOnMultiAttack(2);
 		addToAvailableAttacks(longbow);
 	}
 	
-	private void setSpecialAttacks() {
-		
+	private void setActions() {
+		Action petrifyingGaze = new ActionBuilder()//
+				.setName("petrifying gaze")//
+				.setArea("One creature that can see the medusa and starts its turn within 30ft of it\r\n\r\n")//
+				.setSave(new SavingThrow(14, AbilityTypes.CON))//
+				.setDescription(getPetrifyingGazeText())//
+				.build();
+		addToActions(petrifyingGaze);
+	}
+	
+	private String getPetrifyingGazeText() {
+		return "When a creature that can see the medusa’s eyes starts its turn within 30 feet\r\n"
+				+ " of the medusa, the medusa can force it to make a DC 14 Constitution saving throw\r\n"
+				+ " if the medusa isn’t incapacitated and can see the creature. If the saving throw\r\n"
+				+ " fails by 5 or more, the creature is instantly petrified. Otherwise, a creature\r\n"
+				+ " that fails the save begins to turn to stone and is restrained. The restrained creature\r\n"
+				+ " must repeat the saving throw at the end of its next turn, becoming petrified on a\r\n"
+				+ " failure or ending the effect on a success. The petrification lasts until the\r\n "
+				+ " creature is freed by the greater restoration spell or other magic.\r\n"
+				+ " Unless surprised, a creature can avert its eyes to avoid the saving throw at.\r\n"
+				+ " the start of its turn. If the creature does so, it can’t see the medusa until.\r\n"
+				+ " the start of its next turn, when it can avert its eyes again. If the creature.\r\n"
+				+ " looks at the medusa in the meantime, it must immediately make the save. If the.\r\n"
+				+ " medusa sees itself reflected on a polished surface within 30 feet of it and in.\r\n"
+				+ " an area of bright light, the medusa is, due to its curse, affected by its own gaze.";
 	}
 }
