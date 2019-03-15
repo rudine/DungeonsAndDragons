7ab4b741e1c078cc13446ea7194a32bb700baf6a
Rudine Bijlsma <rudine@gmail.com>
2019-03-15 16:53:05 +0100

    Hippogriff and Bloodhawk for City of Danger - Hatred like clawed wind

---
+++ src/basic/monsters/BloodHawk.java
@@ -0,0 +1,37 @@
+package basic.monsters;
+
+import basic.attack.DamageComponent;
+import basic.attack.types.MeleeAttack;
+import basic.attack.types.builders.MeleeAttackBuilder;
+import basic.monsters.specialabilities.SpecialAbility;
+import basic.ruleobjects.AbilityScores;
+import basic.ruleobjects.AbilityTypes;
+import basic.ruleobjects.DamageType;
+import basic.ruleobjects.Size;
+import basic.ruleobjects.Skill;
+import basic.ruleobjects.modifiers.SkillModifier;
+import basic.services.DiceService;
+
+public class BloodHawk extends AbstractEnemy{
+	
+	public BloodHawk() {
+		setSize(Size.Small);
+		setAC(12);
+		setHitpoints(DiceService.throwD6(2));
+		setSpeed("10ft 2v, 60ft 12v");
+		setAbilityScores(new AbilityScores(6, 14, 10, 3, 14, 5));
+		addToSkills(new SkillModifier(4, Skill.Perception, AbilityTypes.WIS));
+		addToSpecialAbilities(SpecialAbility.PackTactics, SpecialAbility.KeenSight);
+		setAttacks();
+		setAttacksOnAttackAction(1);
+	}
+	
+	private void setAttacks() {
+		MeleeAttack beak = new MeleeAttackBuilder()//
+				.setWeaponName("Beak")//
+				.setToHit(4)//
+				.addDamageComponent(new DamageComponent(1, 4, 2, DamageType.PIERCING))
+				.build();
+		addToAvailableAttacks(beak);
+	}
+}
---
+++ src/basic/monsters/Hippogriff.java
@@ -0,0 +1,45 @@
+package basic.monsters;
+
+import basic.attack.DamageComponent;
+import basic.attack.types.MeleeAttack;
+import basic.attack.types.builders.MeleeAttackBuilder;
+import basic.monsters.specialabilities.SpecialAbility;
+import basic.ruleobjects.AbilityScores;
+import basic.ruleobjects.AbilityTypes;
+import basic.ruleobjects.DamageType;
+import basic.ruleobjects.Size;
+import basic.ruleobjects.Skill;
+import basic.ruleobjects.modifiers.SkillModifier;
+import basic.services.DiceService;
+
+public class Hippogriff extends AbstractEnemy {
+
+	public Hippogriff() {
+		setSize(Size.Large);
+		setAC(11);
+		setHitpoints(DiceService.throwD10(3) + 3);
+		setSpeed("40ft. 8v, fly 60ft. 12v");
+		setAbilityScores(new AbilityScores(17, 13, 13, 2, 12, 8));
+		addToSkills(new SkillModifier(5, Skill.Perception, AbilityTypes.WIS));
+		addToSpecialAbilities(SpecialAbility.KeenSight);
+		setAttacks();
+		setAttacksOnAttackAction(2);
+	}
+	
+	private void setAttacks() {
+		MeleeAttack beak = new MeleeAttackBuilder()//
+				.setWeaponName("beak")//
+				.setToHit(5)//
+				.setUsesOnMultiAttack(1)//
+				.addDamageComponent(new DamageComponent(1, 10, 3, DamageType.PIERCING))
+				.build();
+		addToAvailableAttacks(beak);
+		MeleeAttack claw = new MeleeAttackBuilder()//
+				.setWeaponName("claws")//
+				.setToHit(5)//
+				.setUsesOnMultiAttack(1)//
+				.addDamageComponent(new DamageComponent(2, 6, 3, DamageType.SLASHING))
+				.build();
+		addToAvailableAttacks(claw);
+	}
+}
