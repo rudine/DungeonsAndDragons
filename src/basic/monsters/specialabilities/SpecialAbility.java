b38b95aecf0eab7811837627d75020b99160c8c6
Rudine Bijlsma <rudine@gmail.com>
2019-03-15 16:53:37 +0100

    Extra special abilities

--- src/basic/monsters/specialabilities/SpecialAbility.java
+++ src/basic/monsters/specialabilities/SpecialAbility.java
@@ -3,6 +3,7 @@ package basic.monsters.specialabilities;
 public enum SpecialAbility {
 	
 	Amorphous("Amorphous", "Can move through a space as narrow as 1 inch wide without squeezing."),
+	Beserk("Beserk", "Whenever this enemy starts its turn with 40 hit points or fewer, roll a d6. On a 6, it goes berserk. On each of its turns while berserk, it attacks the nearest creature it can see. If no creature is near enough to move to and attack, the enemy attacks an object, with preference for an object smaller than itself. Once the enemy goes berserk, it continues to do so until it is destroyed or regains all its hit points. The enemies' creator, if within 60 ft. it, can try to calm it by speaking firmly and persuasively. The enemy must be able to hear its creator, who must take an action to make a DC 15 Charisma (Persuasion) check. If the check succeeds, it ceases being berserk. If it takes damage while still at 40 hit points or fewer, it might go berserk again."),
 	Brave("Brave", "Has advantage on saving throws against being frightened."),
 	ConferFireResistance("Confer fire resistance", "The enemy can grant resistance to fire damage to anyone riding it"),
 	CorrosiveForm("Corrosive Form", "A creature that touches this enemy or hits it with a melee attack while within 5ft of it takes 4(1d8) ACID dmg. Any non magical weapon made of wood or metal that hits it corrodes. After dealing damage, the weapon takes a permanent and cumulative -1 penalty to damage rolls. "
@@ -10,7 +11,12 @@ public enum SpecialAbility {
 	FalseAppearance("False appearance", "While the enemy remains motionless, it is indistinguishable from an inanimate statue."),
 	FearOfFire("Fear of fire", "If the enemy takes fire damage, it has disadvantage on attack rolls and ability checks until the end of its next turn"),
 	Illumination("Illumination", "The enemy sheds bright light in a 10ft. radius and dim light for an additional 10ft."),
+	ImmutableForm("Immutable form", "The enemy is immune to any spell or effect that would alter its form."),
+	KeenSight("Keen Sight", "The enemy has advantage on Wisdom (Perception) checks that rely on sight."),
 	KeenSmell("Keen Smell", "Has advantage on Wisdom (Perception) checks that rely on smell."),
+	LightningAbsorption("Lightning absorption", "Whenever this enemy receives lightning damage, it takes no damage and instead regains a number of hit points equal to the damage dealt."),
+	MagicResistance("Magic resistance", "This enemy had advantage on svaing throuws against spells and other magical effects."),
+	MagicWeapons("Magic weapons","The attacks of this enemy are magical"),
 	PackTactics("Pack Tactics", "Has advantage on an attack roll against a creature if at least one of the lion’s allies is within 5 feet of the creature and the ally isn’t incapacitated."),
 	Pounce("Pounce", "If this enemy moves at least 20 feet straight toward a creature and then hits it with a claw attack on the same turn, that target must succeed on a DC 13 Strength saving throw or be knocked prone. If the target is prone, the enemy can make one bite attack against it as a bonus action."),
 	Rampage("Rampage", "If this enemy reduces a creature to 0 hitpoints with a melee attack on its turn, the enemy can take a bonus attack to move up to half its speed and make a bite attack"),
