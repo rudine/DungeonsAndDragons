07d41fdeae165dc8e7e3651b8a7be4bce2cb218c
Rudine Bijlsma <rudine@gmail.com>
2019-03-22 12:51:06 +0100

    AbstractFight for less copy-pasteness

---
+++ src/gui/components/AbstractFight.java
@@ -0,0 +1,71 @@
+package gui.components;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public abstract class AbstractFight extends Application {
+	
+	private ScrollPane scrollPane;
+	protected GridPane basePane;
+	protected int dmInitiative;
+	private String fightTitle;
+	private HBox topPane;
+	private Text turnText;
+	private int turn;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.fightTitle = setFightTitle();
+		initializeBaseAndScrollPane();
+		initializeEnemiesAndEnemyPanes();
+		determineDMInitiative();
+		initializeTopButtons();
+		setStage(primaryStage);
+	}
+	
+	private void initializeBaseAndScrollPane() {
+		this.basePane = new GridPane();
+		basePane.setPrefSize(1850, 1000);
+		this.scrollPane = new ScrollPane(basePane);
+	}
+	
+	private void initializeTopButtons() {
+		topPane = new HBox();
+		topPane.setPadding(new Insets(10, 10, 10, 10));
+		topPane.setSpacing(10);
+
+		Button turnButton = new Button("Next turn");
+		turnButton.setOnAction(e -> onNextTurn());
+		turnText = new Text("Turn number: " + turn);
+		topPane.getChildren().addAll(turnButton, new Text("DM initiative: " + dmInitiative), turnText);
+
+		basePane.add(topPane, 0, 0);
+	}
+	
+	private void onNextTurn() {
+		turnText.setText("Turn number: " + ++turn);
+		nextTurn();
+	}
+	
+	protected abstract String setFightTitle();
+	
+	protected abstract void nextTurn();
+	
+	protected abstract void determineDMInitiative();
+	
+	protected abstract void initializeEnemiesAndEnemyPanes();
+	
+	private void setStage(Stage primaryStage) {
+		Scene scene = new Scene(scrollPane);
+		primaryStage.setTitle(fightTitle);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
--- src/gui/ddex/cityofdanger/hatred/BloodHawksFight.java
+++ src/gui/ddex/cityofdanger/hatred/BloodHawksFight.java
@@ -8,39 +8,53 @@ import basic.monsters.AbstractEnemy;
 import basic.monsters.BloodHawk;
 import basic.monsters.Hippogriff;
 import basic.services.StandardRulesService;
+import gui.components.AbstractFight;
 import gui.components.EnemyPane;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
-public class BloodHawksFight extends Application {
+public class BloodHawksFight extends AbstractFight {
 
-	private ScrollPane scrollpane;
 	private List<BloodHawk> bloodHawks;
-	private GridPane basePane;
-	private int dmInitiative;
 	private List<EnemyPane<BloodHawk>> bloodHawkPanes;
 	private EnemyPane<Hippogriff> hippogriffPane;
-	private String fightTitle = "Fight on the top of the tower, near the shrine of Talos";
 	private int numberOfBloodhawks = 8;
 	private boolean includeHippogriff = false;
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		this.basePane = new GridPane();
-		basePane.setPrefSize(1850, 1000);
-		this.scrollpane = new ScrollPane(basePane);
+	protected String setFightTitle() {
+		return "Fight on the top of the tower, near the shrine of Talos";
+	}
 
+	@Override
+	protected void nextTurn() {
+		for (EnemyPane<BloodHawk> bloodhawkPane : bloodHawkPanes) {
+			nextTurnActions(bloodhawkPane);
+		}
+		nextTurnActions(hippogriffPane);
+	}
+	
+	private <T extends AbstractEnemy> void nextTurnActions(EnemyPane<T> pane) {
+		if (pane != null) {
+			pane.refreshAttackPanes(false);
+			pane.refreshHeader();
+			pane.refreshCheckBoxes();
+		}
+	}
+
+	@Override
+	protected void determineDMInitiative() {
+		List<Integer> dextMods = bloodHawks.stream().map(h -> h.getAbilityScores().getDexterityModifier())
+				.collect(Collectors.toList());
+		dextMods.add(new Hippogriff().getAbilityScores().getDexterityModifier());
+		dmInitiative = StandardRulesService.determineDMInitiative(dextMods);
+	}
+
+	@Override
+	protected void initializeEnemiesAndEnemyPanes() {
 		bloodHawks = new ArrayList<>();
 		bloodHawkPanes = new ArrayList<>();
-		
-		for(int i = 0; i < numberOfBloodhawks; i++) {
+
+		for (int i = 0; i < numberOfBloodhawks; i++) {
 			BloodHawk bloodhawk = new BloodHawk();
 			bloodHawks.add(bloodhawk);
 			EnemyPane<BloodHawk> bloodhawkPane = new EnemyPane<>(bloodhawk);
@@ -48,53 +62,11 @@ public class BloodHawksFight extends Application {
 			int location = includeHippogriff ? i + 1 : i;
 			basePane.add(bloodhawkPane, location, 1);
 		}
-		
-		if(includeHippogriff) {
+
+		if (includeHippogriff) {
 			hippogriffPane = new EnemyPane<Hippogriff>(new Hippogriff());
 			basePane.add(hippogriffPane, 0, 1);
 		}
-
-		determineDMInitiative();
-		initializeTopButtons();
-		setStage(primaryStage);
-	}
-
-	private void determineDMInitiative() {
-		List<Integer> dextMods = bloodHawks.stream().map(h -> h.getAbilityScores().getDexterityModifier()).collect(Collectors.toList());
-		dextMods.add(new Hippogriff().getAbilityScores().getDexterityModifier());
-		dmInitiative = StandardRulesService.determineDMInitiative(dextMods);
-	}
-
-	private void initializeTopButtons() {
-		HBox topPane = new HBox();
-		topPane.setPadding(new Insets(10, 10, 10, 10));
-		topPane.setSpacing(10);
-
-		Button turnButton = new Button("Next turn");
-		turnButton.setOnAction(e -> nextTurn());
-		topPane.getChildren().addAll(turnButton, new Text("DM initiative: " + dmInitiative));
-
-		basePane.add(topPane, 0, 0);
-	}
-	
-	private void nextTurn() {
-		for(EnemyPane<BloodHawk> bloodhawkPane: bloodHawkPanes) {
-			nextTurnActions(bloodhawkPane);
-		}
-		nextTurnActions(hippogriffPane);
-	}
-
-	private <T extends AbstractEnemy> void nextTurnActions(EnemyPane<T> pane) {
-		pane.refreshAttackPanes(false);
-		pane.refreshHeader();
-		pane.refreshCheckBoxes();
-	}
-	
-	private void setStage(Stage primaryStage) {
-		Scene scene = new Scene(scrollpane);
-		primaryStage.setTitle(fightTitle);
-		primaryStage.setScene(scene);
-		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
